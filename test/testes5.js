e9b510457eacc69d2a17e889f7cad6f4800ad7b4
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-11-13 23:24:46 +0800

    update

--- test/testes5.js
+++ test/testes5.js
@@ -2,6 +2,47 @@ var {
   distict
 } = require('../src/es5');
 
-var arr = [1, '1', 2, '2', 3, '3', 4, '4'];
+// var arr = [1, '1', 2, '2', 3, '3', 4, '4'];
 
-console.log(distict(arr));
+// console.log(distict(arr));
+
+const app = {
+  start: function () {
+    console.log('start');
+    this.end();
+  },
+  end: function () {
+    console.log('end')
+  },
+  holder: function () {
+    console.log('holder==>', this);
+    console.log('insideApp');
+  }
+}
+
+const app2 = {
+  temp: 'temp'
+}
+const oldAppStart = app.start;
+
+const start = () => {
+  oldAppStart.call(app);
+  console.log('start2');
+}
+
+const holder = (txt) => {
+  console.log('this==>', this);
+  console.log(txt);
+  return (txt2) => console.log(txt2);
+}
+console.log('thisX==>', this);
+// console.log('outside==>', this);
+app2.holder = app.holder.bind(null, 'hello world');
+
+app.start = start;
+
+
+// oldAppStart.call(app2)
+
+// app.start()
+// app2.holder();
--- test/testes6.js
+++ test/testes6.js
@@ -1,21 +1,29 @@
-import {
-  father,
-  son,
-  sum,
-  distict
-}  from '../src/es6';
+// import {
+//   father,
+//   son,
+//   sum,
+//   distict
+// }  from '../src/es6';
 
 
-const arr = [1, '1', 2, '2', 3, '3', 4, '4'];
+// const arr = [1, '1', 2, '2', 3, '3', 4, '4'];
 
-console.log(distict(arr));
+// console.log(distict(arr));
 
-console.log('sum==>', es6.sum(1,2,3,4,5,6,7,8,9,10))
+// console.log('sum==>', es6.sum(1,2,3,4,5,6,7,8,9,10))
 
-console.log('fatherAge==>', father.age);
-father.work();
-console.log('sonAge==>',son.age);
-console.log('sonlife==>',son.life);
-son.work();
-son.play();
-console.log(son.age);
+// console.log('fatherAge==>', father.age);
+// father.work();
+// console.log('sonAge==>',son.age);
+// console.log('sonlife==>',son.life);
+// son.work();
+// son.play();
+// console.log(son.age);
+import Test2 from '../src/classTest2';
+
+const t2 = new Test2();
+
+t2.say();
+
+// const t1 = new Test();
+// t1.say()

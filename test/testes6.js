fe499fabce5d2d07ad83284b4300163535ff939e
欧阳夏昱 <ouyangxiayu@m24.cn>
2017-11-28 00:28:51 +0800

    update

--- test/testes6.js
+++ test/testes6.js
@@ -1,73 +1,22 @@
-// import thisDemo from '../src/thisDemo.js';
-// import thisDemoObj from '../src/thisDemoObj.js';
-// // // thisDemo();
-// thisDemoObj.thisDemo();
-
-// function taskA() {
-//   this.name = 'hello'
-
-//   var fn = function() {
-//     console.log(this)
-//     console.log(this.name)
-//   }
-
-//   var arrow_fn = () => {
-//     console.log(this)
-//     console.log(this.name)
-//   }
-//   fn()
-//   arrow_fn()
-// }
-// taskA()
-
-// const obj = {
-//   field: 'hello',
-//   getField: function ()  {
-//     console.log(this.field)
-//   }
-// }
-// obj.getField()
-
-// [1, 2, 3, 4, 5, 6, 7].reduce((preValue, current, currIndex, arr) => {
-
-//   console.log('preValue==>', preValue);
-//   console.log('current==>', current);
-//   console.log('currIndex==>', currIndex);
-//   console.log('arr==>', arr);
-//   preValue[currIndex] = current;
-//   return preValue;
-// }, {})
-
-// const arr = [1, 2, 3, 4, 5, 6, 7];
-
-// let s = 1;
-
-// arr.map( item => {
-//   s *= item;
-// })
-
-// console.log('s==>', s)
-
-// console.log('s==>',arr.reduce((pre, cur) => pre * cur, 1));
-
-// const str = 'hello, world!';
-
-// const reg = /([a-z]||[A-Z])+/g;
-
-// const chars = str.match(reg).reduce( (pre, cur) => !!cur ? pre.concat(cur):pre, [] );
-
-// console.log('chars==>', chars.reverse());
-
-
-// const b = 2;
-// const a = {
-//   b: 1,
-//   c: () => {
-//     console.log(this.b);
-//   }
-//   // c: function(){
-//   //   console.log(this.b);
-//   // }
+import  window from "global/window";
+import global from "global";
+import document from "global/document";
+// var window = require("global/window")
+// const Ajax = {
+//   get: function(url, fn) {
+//     const httpUtils = window.XMLHttpRequest
+//     httpUtils.open('GET', url, true);
+//     httpUtils.onreadystatechange = function() {
+//       if(httpUtils.readyState === 4 && httpUtils.status === 200 || httpUtils.status === 304) {
+//         fn.call(this, httpUtils.responseText);
+//       }
+//     }
+//     httpUtils.send();
+//   } 
 // }
 
-// a.c();
+
+console.log('window==>', window.toString());
+console.log('global==>', global.XMLHttpRequest);
+console.log('document==>', document.XMLHttpRequest);
+// Ajax.get(`http://localhost:3000/users?id=123&name=cat`)
